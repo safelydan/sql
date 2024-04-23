bb53d1a6f61d2d1c6b24971ab1e5bdb6609c0e5a
daniel <daniellealuau@gmail.com>
2024-04-23 00:07:30 -0300

    variaveis

--- db.js
+++ db.js
@@ -1,9 +1,17 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import dotenv from 'dotenv';
 
-import Sequelize from 'sequelize';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const sequelize = new Sequelize('postgres', 'postgres', 'hyperballad', {
-  host: 'localhost',
+dotenv.config({ path: `${__dirname}/.env` });
+
+import { Sequelize } from 'sequelize';
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'postgres'
 });
+
 
 export default sequelize;
